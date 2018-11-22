902c07006dd38e768604cc200fa126fdf45c6cbc
1247819023 <liang2361928131@qq.com>
2018-11-22 08:30:48 +0800

    vue-element

---
+++ src/components/comment/comment-list.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='clist'>
+        <comment v-for='c in comments'
+                 :comment='c'
+                 :key='c.id'
+                 @deletes='doDelete'>
+        </comment>
+    </div>
+</template>
+
+<script>
+ import commentItem from './comment-item';
+
+ export default {
+     props: ['comments'],
+     methods: {
+         doDelete(id) {
+             this.$emit('deletes', id);
+         }
+     },
+     components: {
+         comment: commentItem
+     }
+ };
+</script>
